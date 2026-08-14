cbf95348074e82e065625ff1e63cccb552be5427
a <a@a.com>
2026-08-14 08:22:42 -0700

    cleanup: merge devil_dogs into 2lt_devil_dog, rm dupe cpt_eco.py, consolidate gitignore

--- ecosystem/cpt_eco/cpt_eco.py
+++
@@ -1,7 +0,0 @@
-class CptEco:
-    def __init__(self):
-        self.id = "cpt_eco"
-        self.rank = 9
-        self.role = "Manager - project execution"
-    def execute(self, task):
-        return f"[CPT 9] Execute: {task}"
